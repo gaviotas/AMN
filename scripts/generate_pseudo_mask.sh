7f612d00e434eab7766ec517473cdf1b9d639192
Minhyun Lee <lmh315@yonsei.ac.kr>
2022-04-25 19:30:31 +0900

    Update generate_pseudo_mask.sh

--- scripts/generate_pseudo_mask.sh
+++ scripts/generate_pseudo_mask.sh
@@ -4,7 +4,7 @@ GPU=0
 
 # 1. train a classification network and compute refined seed
 # CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-#     --voc12_root {DATASET_ROOT} \
+#     --voc12_root ${DATASET_ROOT} \
 #     --num_workers 8 \
 #     --train_cam_pass True
 #     --cam_eval_thres 0.15 \
@@ -16,13 +16,13 @@ GPU=0
 
 # 2.1. train an attribute manipulation network
 # CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-#     --voc12_root {DATASET_ROOT} \
+#     --voc12_root ${DATASET_ROOT} \
 #     --num_workers 8 \
 #     --train_amn_pass True
 
 # 2.2. generate activation maps and refined seed for boundary refinement
 CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-    --voc12_root {DATASET_ROOT} \
+    --voc12_root ${DATASET_ROOT} \
     --num_workers 8 \
     --make_amn_cam_pass True \
     --eval_amn_cam_pass True \
@@ -32,13 +32,13 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
 
 # 3.1. train a boundary refinement network (IRN)
 # CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-#     --voc12_root {DATASET_ROOT} \
+#     --voc12_root ${DATASET_ROOT} \
 #     --num_workers 8 \
 #     --train_irn_pass True
 
 # 3.2. generate the final pseudo-masks
 CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-    --voc12_root {DATASET_ROOT} \
+    --voc12_root ${DATASET_ROOT} \
     --num_workers 8 \
     --make_sem_seg_pass True \
     --eval_sem_seg_pass True
