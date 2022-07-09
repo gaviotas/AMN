a530eddf99ffe978e8bf195e0a71e8f8a6153f9b
Minhyun Lee <lmh315@yonsei.ac.kr>
2022-07-09 12:51:45 +0900

    updates scripts

--- scripts/generate_pseudo_mask.sh
+++ scripts/generate_pseudo_mask.sh
@@ -1,28 +1,17 @@
 # NEED TO SET
-DATASET_ROOT=PATH/TO/DATASET
-GPU=0
-
-# 1. train a classification network and compute refined seed
-# CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-#     --voc12_root ${DATASET_ROOT} \
-#     --num_workers 8 \
-#     --train_cam_pass True
-#     --cam_eval_thres 0.15 \
-#     --make_cam_pass True \
-#     --eval_cam_pass True \
-#     --cam_to_ir_label_pass True \
-#     --conf_fg_thres 0.30 \
-#     --conf_bg_thres 0.05
+# DATASET_ROOT=PATH/TO/DATASET
+DATASET_ROOT=/data/coco_2014
+GPU=0,1,2,3
 
 # 2.1. train an attribute manipulation network
-# CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-#     --voc12_root ${DATASET_ROOT} \
+# CUDA_VISIBLE_DEVICES=0 python3 run_sample_coco.py \
+#     --coco14_root ${DATASET_ROOT} \
 #     --num_workers 8 \
 #     --train_amn_pass True
 
 # 2.2. generate activation maps and refined seed for boundary refinement
-CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-    --voc12_root ${DATASET_ROOT} \
+CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample_coco.py \
+    --coco14_root ${DATASET_ROOT} \
     --num_workers 8 \
     --make_amn_cam_pass True \
     --eval_amn_cam_pass True \
@@ -31,14 +20,14 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
     --conf_bg_thres 0.15
 
 # 3.1. train a boundary refinement network (IRN)
-# CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-#     --voc12_root ${DATASET_ROOT} \
+# CUDA_VISIBLE_DEVICES=0 python3 run_sample_coco.py \
+#     --coco14_root ${DATASET_ROOT} \
 #     --num_workers 8 \
 #     --train_irn_pass True
 
 # 3.2. generate the final pseudo-masks
-CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
-    --voc12_root ${DATASET_ROOT} \
+CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample_coco.py \
+    --coco14_root ${DATASET_ROOT} \
     --num_workers 8 \
     --make_sem_seg_pass True \
     --eval_sem_seg_pass True
